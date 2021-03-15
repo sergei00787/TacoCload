370bb99bcef9159aa1b86e9768be21d8353dfcba
sergei00787 <sergei007_87@mail.ru>
2021-03-15 16:58:17 +0700

    Add taco repository

--- src/main/java/sia/tacocloud/Taco.java
+++ src/main/java/sia/tacocloud/Taco.java
@@ -17,6 +17,6 @@ public class Taco {
     @Size(min=5, message="Name must be at least 5 characters long")
     private String name;
     @Size(min=1, message = "You must choose ta least 1 ingredient")
-    private List<String> ingredients;
+    private List<Ingredient> ingredients;
 }
 
